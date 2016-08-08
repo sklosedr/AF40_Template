cdc4a5017434b9d23b69b3abaf9063bf83ac980f
Martin Zimmermann <mzimmermann@next-level-integration.com>
2016-08-08 22:01:12 +0200

    backend url app3
    Change-Id: Ia599cea1e3b009a9d857494829d3ca9502e0cee3

--- af40-template-webapp/src/main/resources/static/app/index.module.js
+++ af40-template-webapp/src/main/resources/static/app/index.module.js
@@ -5,8 +5,8 @@
     angular.module('fastTrackApp', ['ngMaterial', 'fastTrackApp.businessPartner'])
     	.config(config);
     
-//    angular.module('fastTrackApp').constant("baseUrl", "http://app3.level365.de");
-    angular.module('fastTrackApp').constant("baseUrl", "http://localhost:8080");
+    angular.module('fastTrackApp').constant("baseUrl", "http://app3.level365.de");
+//    angular.module('fastTrackApp').constant("baseUrl", "http://localhost:8080");
     
     /** @ngInject */
     function config()
