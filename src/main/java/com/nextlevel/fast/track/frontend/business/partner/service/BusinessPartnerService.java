c442c42dd5e7aba5a9a1065e83be9937faf30da9
Martin Zimmermann <mzimmermann@next-level-integration.com>
2016-08-08 14:55:14 +0200

    use separate configuration files for different spring profiles
    Change-Id: I534771aad7bfc93880960867ec4af50047e0ca20

--- src/main/java/com/nextlevel/fast/track/frontend/business/partner/service/BusinessPartnerService.java
+++ src/main/java/com/nextlevel/fast/track/frontend/business/partner/service/BusinessPartnerService.java
@@ -2,6 +2,7 @@ package com.nextlevel.fast.track.frontend.business.partner.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,14 +11,17 @@ import com.nextlevel.fast.track.frontend.business.partner.model.BusinessPartner;
 @Service
 public class BusinessPartnerService {
 	
+	@Value("${af40.backend.url}")
+	private String backendUrl;
+	
 	private RestTemplate restTemplate = new RestTemplate();
 	
 	public void createBusinessPartner(BusinessPartner businessPartner) {
-		restTemplate.postForEntity("http://104.155.54.166/businessPartner", businessPartner, BusinessPartner.class);
+		restTemplate.postForEntity(backendUrl + "/businessPartner", businessPartner, BusinessPartner.class);
 	}
 	
 	public List<BusinessPartner> getBusinessPartner() {
-		return restTemplate.getForObject("http://104.155.54.166/businessPartner", List.class);
+		return restTemplate.getForObject(backendUrl + "/businessPartner", List.class);
 	}
 
 }
