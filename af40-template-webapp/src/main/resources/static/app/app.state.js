2be8b0e9fcbd1aa7f22570193bd7d7e83f28e647
Martin Zimmermann <mzimmermann@next-level-integration.com>
2016-08-09 11:05:31 +0200

    naming conventions for html templates
    Change-Id: I64c7c7d0a600b5126358f24fb821be571ecbd948

--- af40-template-webapp/src/main/resources/static/app/app.state.js
+++ af40-template-webapp/src/main/resources/static/app/app.state.js
@@ -6,12 +6,12 @@
     angular.module('fastTrackApp').config(function($routeProvider) {
     	$routeProvider
     		.when('/', {
-    			templateUrl: 'app/wizard/wizard.html',
+    			templateUrl: 'app/wizard/wizard.view.html',
         		controller: 'WizardController',
         		controllerAs: 'vm'    			
     		})
     		.when('/login', {
-    			templateUrl: 'app/login/login.html',
+    			templateUrl: 'app/login/login.view.html',
     			controller: 'LoginController',
     			controllerAs: 'vm'
     		})
