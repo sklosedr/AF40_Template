1065f76ef65097e6ee59309b82e57dfcc900b3a6
Martin Zimmermann <mzimmermann@next-level-integration.com>
2016-08-23 17:08:40 +0200

    update constant baseURL for live environment
    Change-Id: I029c0c7bf82ed7f463728969a9241d3a1d9f268f

--- af40-template-webapp/src/main/resources/static/app/app.config.js
+++ af40-template-webapp/src/main/resources/static/app/app.config.js
@@ -2,7 +2,7 @@
 {
     'use strict';
     
-    angular.module('fastTrackApp').constant("baseUrl", "http://104.155.54.166");
+    angular.module('fastTrackApp').constant("baseUrl", "http://23.251.139.27");
 //    angular.module('fastTrackApp').constant("baseUrl", "http://localhost:8080");
     
     angular.module('fastTrackApp').config(function($httpProvider) {
