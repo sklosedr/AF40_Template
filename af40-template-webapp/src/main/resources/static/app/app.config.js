2e12761b0a064390ed4c4f9542f8aa11f8ef2788
Martin Zimmermann <mzimmermann@next-level-integration.com>
2016-09-06 20:38:05 +0200

    use google cloud url for rest services
    Change-Id: I08cb38551a417f598daf254ab5698b3cfda2ae46

--- af40-template-webapp/src/main/resources/static/app/app.config.js
+++ af40-template-webapp/src/main/resources/static/app/app.config.js
@@ -2,8 +2,8 @@
 {
     'use strict';
     
-//    angular.module('fastTrackApp').constant("baseUrl", "http://23.251.139.27");
-    angular.module('fastTrackApp').constant("baseUrl", "http://localhost:8080");
+    angular.module('fastTrackApp').constant("baseUrl", "http://23.251.139.27");
+//    angular.module('fastTrackApp').constant("baseUrl", "http://localhost:8080");
     
     angular.module('fastTrackApp').config(function($httpProvider, $mdThemingProvider) {
     	$httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
