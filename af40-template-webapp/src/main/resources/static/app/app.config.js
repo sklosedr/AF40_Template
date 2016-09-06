48ea87f7c310d4050c701d60b98db997da320f69
Martin Zimmermann <mzimmermann@next-level-integration.com>
2016-09-06 20:34:32 +0200

    config angular material default theme
    Change-Id: I8a1cd0b601d0127cfd82e4399eef8ead38fdefa0

--- af40-template-webapp/src/main/resources/static/app/app.config.js
+++ af40-template-webapp/src/main/resources/static/app/app.config.js
@@ -2,11 +2,16 @@
 {
     'use strict';
     
-    angular.module('fastTrackApp').constant("baseUrl", "http://23.251.139.27");
-//    angular.module('fastTrackApp').constant("baseUrl", "http://localhost:8080");
+//    angular.module('fastTrackApp').constant("baseUrl", "http://23.251.139.27");
+    angular.module('fastTrackApp').constant("baseUrl", "http://localhost:8080");
     
-    angular.module('fastTrackApp').config(function($httpProvider) {
+    angular.module('fastTrackApp').config(function($httpProvider, $mdThemingProvider) {
     	$httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+    	$mdThemingProvider.theme('default')
+	    	.primaryPalette('amber')
+	        .accentPalette('light-blue')
+	        .warnPalette('deep-orange')
+	        .backgroundPalette('grey'); 
     });
 
 })();
