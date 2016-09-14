13e4d3a53ce5aaca91205c4b471771bb26c16499
ksheng <ksheng@NBNLI00512.nli.local>
2016-09-14 10:25:34 +0200

    made scroll auto

--- af40-template-webapp/src/main/resources/static/app/app.config.js
+++ af40-template-webapp/src/main/resources/static/app/app.config.js
@@ -3,7 +3,7 @@
     'use strict';
     
           angular.module('fastTrackApp').constant("baseUrl", "http://app3.level365.de/");
- //		angular.module('fastTrackApp').constant("baseUrl", "http://localhost:8080");
+    //    angular.module('fastTrackApp').constant("baseUrl", "http://localhost:8080");
     
     angular.module('fastTrackApp').config(function($httpProvider, $mdThemingProvider) {
     	$httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
