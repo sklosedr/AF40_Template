1c53bfbeba0d9560ea937ffab7cf9f7907ca8702
Martin Zimmermann <mzimmermann@next-level-integration.com>
2016-11-16 22:08:11 +0100

    customer1 customizing
    Change-Id: Ie102f4d757ec4e408bd2e3d3ae9f79176dc998d9

---
+++ af40-frontend/af40-customer-frontend/af40-customer1-frontend/src/main/resources/static/app/app.config.js
@@ -0,0 +1,18 @@
+(function ()
+{
+    'use strict';
+
+    angular.module('fastTrackApp').config(globalConfig);
+    
+    globalConfig.$inject = ['$httpProvider', '$mdThemingProvider'];
+    
+    function globalConfig($httpProvider, $mdThemingProvider) {
+    	$httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+    	$mdThemingProvider.theme('default')
+	    	.primaryPalette('orange')
+	        .accentPalette('blue')
+	        .warnPalette('deep-orange')
+	        .backgroundPalette('grey'); 
+    };
+
+})();
---
+++ af40-frontend/af40-customer-frontend/af40-customer1-frontend/src/main/resources/static/app/app.module.js
@@ -0,0 +1,17 @@
+(function ()
+{
+    'use strict';
+    
+    angular
+    	.module('fastTrackApp', [
+    	  'ngStorage',
+    	  'ngCookies',
+    	  'mdSteppers',
+    	  'ngMaterial',
+    	  'ngResource',
+    	  'ngRoute',
+    	  'base64',
+    	  'uiGmapgoogle-maps'
+    	  ]);
+
+})();
